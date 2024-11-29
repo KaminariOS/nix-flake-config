074780bb1c2e23b0074782c4f218b3ddb3486e28
kosumi <chankocyo@gmail.com>
2024-11-28 23:55:02 -0500

    Need to have wl-clipboard for copy paste to work

--- flake.nix
+++ flake.nix
@@ -6,6 +6,13 @@
 
     nurpkgs.url = "github:nix-community/NUR";
 
+    # for macos
+    # nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-24.11-darwin";
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
