2ada9efcfe64f1e9d688eb2f9504ada4fc6b12ea
kosumi <chankocyo@gmail.com>
2024-11-28 23:35:09 -0500

    For unknown reason wl-clipboard-rs does not work

--- home/gui.nix
+++ home/gui.nix
@@ -45,7 +45,7 @@
 
     # wayland stuff
     mako
-    wl-clipboard-rs
+    # wl-clipboard-rs
     shotman
 
     swayidle #idle locker
--- home/services/sway/scripts.nix
+++ home/services/sway/scripts.nix
@@ -6,7 +6,7 @@
 }: let
   swaymsg = lib.getExe' config.wayland.windowManager.sway.package "swaymsg";
 in {
-  screenshotScript = pkgs.writeShellScriptBin "wshot" ''${lib.getExe pkgs.wayshot} -s "$(${lib.getExe pkgs.slurp} -d)"  --stdout | tee /tmp/screenshot.png | ${lib.getExe' pkgs.wl-clipboard-rs "wl-copy"}'';
+  screenshotScript = pkgs.writeShellScriptBin "wshot" ''${lib.getExe pkgs.wayshot} -s "$(${lib.getExe pkgs.slurp} -d)"  --stdout | tee /tmp/screenshot.png | ${lib.getExe' pkgs.wl-clipboard "wl-copy"}'';
   clamshell = pkgs.writeShellScript "sway-clamshell" ''
     docked() {
      	[ "$(${swaymsg} -t get_outputs | ${lib.getExe pkgs.jq} '. | length')" -ne 1 ] && return 0
