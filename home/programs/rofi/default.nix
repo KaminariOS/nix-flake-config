be2f3b273fcaf5f1141d86739bf15142978a7cb8
kosumi <chankocyo@gmail.com>
2025-09-20 21:30:25 -0400

    Update lock

--- home/programs/rofi/default.nix
+++ home/programs/rofi/default.nix
@@ -5,7 +5,6 @@
 }: {
   programs.rofi = {
     enable = true;
-    package = pkgs.rofi-wayland;
     terminal = "${pkgs.wezterm}/bin/wezterm";
     theme = lib.mkForce ./launcher/type2-style-10.rasi;
   };
