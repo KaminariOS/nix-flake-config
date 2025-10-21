32a49955f2787eccd72e8ba1a20a5789f35f719f
kosumi <chankocyo@gmail.com>
2025-10-21 11:55:13 -0400

    Open kubelet port

--- system/configuration.nix
+++ system/configuration.nix
@@ -289,6 +289,7 @@ in {
     allowedTCPPorts = [
       6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
       2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+      10250 # Kubelet
       2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
       22 # ssh
     ];
