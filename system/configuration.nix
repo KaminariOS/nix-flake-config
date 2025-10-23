fc5797157f9d0231add41ca1bc2ca9fbae6d3b09
kosumi <chankocyo@gmail.com>
2025-10-22 22:08:29 -0400

    Enable systemd resolved

--- system/configuration.nix
+++ system/configuration.nix
@@ -169,6 +169,13 @@ in {
     pipewire.wireplumber = {
       enable = true;
     };
+    resolved = {
+      enable = true;
+      dnssec = "true";
+      domains = ["~."];
+      fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+      dnsovertls = "true";
+    };
     auto-cpufreq.enable = true;
     xserver = {
       videoDrivers = ["displaylink" "modesetting"];
