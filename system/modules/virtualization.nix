42c8ce13dd297f2530f748af172101089140c6e7
kosumi <chankocyo@gmail.com>
2026-03-07 19:21:59 -0500

    fix: override libvirt secret init service shell path

--- system/modules/virtualization.nix
+++ system/modules/virtualization.nix
@@ -1,4 +1,9 @@
-{...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   virtualisation.libvirtd.qemu.swtpm.enable = true;
 
   virtualisation = {
@@ -15,6 +20,22 @@
         enable = true;
         dates = "weekly";
       };
+    };
+  };
+
+  systemd.services.virt-secret-init-encryption = {
+    description = "Initialize libvirt secret encryption key";
+    before = ["virtsecretd.service" "libvirtd.service"];
+    startLimitBurst = 3;
+    startLimitIntervalSec = 30;
+    unitConfig.ConditionPathExists = "!/var/lib/libvirt/secrets/secrets-encryption-key";
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = lib.mkForce [
+        ""
+        "${pkgs.runtimeShell} -c 'umask 0077 && (${lib.getExe' pkgs.coreutils "dd"} if=/dev/random status=none bs=32 count=1 | ${config.systemd.package}/bin/systemd-creds encrypt --name=secrets-encryption-key - /var/lib/libvirt/secrets/secrets-encryption-key)'"
+      ];
     };
   };
 }
